46f15253971821f8d2f46d8b2c3c50126b03e271
sumaya hassan <sumaya.alsuhimi@outlook.com>
2026-08-15 17:04:04 +0300

    Fix WebRTC indentation

--- app.py
+++ app.py
@@ -557,46 +557,46 @@ def render_home():
         </div>
         """)
 
-       webrtc_ctx = webrtc_streamer(
-    key="eyemorse-camera",
+        webrtc_ctx = webrtc_streamer(
+            key="eyemorse-camera",
 
-    video_processor_factory=EyeMorseProcessor,
+            video_processor_factory=EyeMorseProcessor,
 
-    rtc_configuration={
-        "iceServers": [
-            {
-                "urls": [
-                    "stun:stun.l.google.com:19302"
+            rtc_configuration={
+                "iceServers": [
+                    {
+                        "urls": [
+                            "stun:stun.l.google.com:19302"
+                        ]
+                    }
                 ]
-            }
-        ]
-    },
-
-    media_stream_constraints={
-        "video": {
-            "width": {
-                "ideal": 480
             },
 
-            "height": {
-                "ideal": 360
+            media_stream_constraints={
+                "video": {
+                    "width": {
+                        "ideal": 480
+                    },
+
+                    "height": {
+                        "ideal": 360
+                    },
+
+                    "frameRate": {
+                        "ideal": 24,
+                        "max": 30
+                    },
+
+                    "facingMode": "user"
+                },
+
+                "audio": False
             },
 
-            "frameRate": {
-                "ideal": 24,
-                "max": 30
-            },
+            desired_playing_state=True,
 
-            "facingMode": "user"
-        },
-
-        "audio": False
-    },
-
-    desired_playing_state=True,
-
-    async_processing=True
-)
+            async_processing=True
+        )
 
     with status_col:
         st.html(
